3d9d9f8d2878bac1855f934248fc3c576bd3341a
Shailesh Sangle <sangleshailesh30@gmail.com>
2021-12-11 00:32:47 +0530

    add business form OTP section

--- Multi steps form/business.js
+++ Multi steps form/business.js
@@ -1,4 +1,8 @@
 const slidePage = document.querySelector(".slide-page");
+const welcomeStart = document.querySelector(".welcome-start");
+const mobileSubmit = document.querySelector(".mobile-submit");
+const backButton = document.querySelector(".back-button");
+const otpSubmit = document.querySelector(".otp-submit");
 const letsStart = document.querySelector(".lets-start");
 const nextBtnFirst = document.querySelector(".firstNext");
 const prevBtnSec = document.querySelector(".prev-1");
@@ -12,9 +16,48 @@ const progressCheck = document.querySelectorAll(".step .check");
 const bullet = document.querySelectorAll(".step .bullet");
 let current = 1;
 
-letsStart.addEventListener("click", function(event){
+welcomeStart.addEventListener("click", function (event) {
   event.preventDefault();
-  slidePage.style.marginLeft = "-22.2%";
+  slidePage.style.marginLeft = "-13.3%";
+  // bullet[current - 1].classList.add("active");
+  // progressCheck[current - 1].classList.add("active");
+  // progressText[current - 1].classList.add("active");
+  // current += 1;
+  // $("#nav-progress-bar").removeClass("navbar-disable-address");
+  // $(".first-bullet-status").addClass("bullet-first");
+  // $(".first-para-status").addClass("step-first");
+});
+mobileSubmit.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-30.7%";
+  // bullet[current - 1].classList.add("active");
+  // progressCheck[current - 1].classList.add("active");
+  // progressText[current - 1].classList.add("active");
+  // current += 1;
+  // $("#nav-progress-bar").removeClass("navbar-disable-address");
+  // $(".first-bullet-status").addClass("bullet-first");
+  // $(".first-para-status").addClass("step-first");
+});
+
+backButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-13.3%";
+});
+
+otpSubmit.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-54.4%";
+  // bullet[current - 1].classList.add("active");
+  // progressCheck[current - 1].classList.add("active");
+  // progressText[current - 1].classList.add("active");
+  // current += 1;
+  // $("#nav-progress-bar").removeClass("navbar-disable-address");
+  // $(".first-bullet-status").addClass("bullet-first");
+  // $(".first-para-status").addClass("step-first");
+});
+letsStart.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-88.86%";
   // bullet[current - 1].classList.add("active");
   // progressCheck[current - 1].classList.add("active");
   // progressText[current - 1].classList.add("active");
@@ -24,9 +67,9 @@ letsStart.addEventListener("click", function(event){
   $(".first-para-status").addClass("step-first");
 });
 
-nextBtnFirst.addEventListener("click", function(event){
+nextBtnFirst.addEventListener("click", function (event) {
   event.preventDefault();
-  slidePage.style.marginLeft = "-54.16%";
+  slidePage.style.marginLeft = "-131.92%";
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
@@ -35,9 +78,9 @@ nextBtnFirst.addEventListener("click", function(event){
   $(".second-para-status").addClass("step-first");
 });
 
-nextBtnSec.addEventListener("click", function(event){
+nextBtnSec.addEventListener("click", function (event) {
   event.preventDefault();
-  slidePage.style.marginLeft = "-79.16%";
+  slidePage.style.marginLeft = "-156.925%";
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
@@ -45,9 +88,9 @@ nextBtnSec.addEventListener("click", function(event){
   $(".third-bullet-status").addClass("bullet-first");
   $(".third-para-status").addClass("step-first");
 });
-nextBtnThird.addEventListener("click", function(event){
+nextBtnThird.addEventListener("click", function (event) {
   event.preventDefault();
-  slidePage.style.marginLeft = "-104.16%";
+  slidePage.style.marginLeft = "-181.93%";
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
@@ -55,15 +98,15 @@ nextBtnThird.addEventListener("click", function(event){
   $(".fourth-bullet-status").addClass("bullet-first");
   $(".fourth-para-status").addClass("step-first");
 });
-submitBtn.addEventListener("click", function(event){
+submitBtn.addEventListener("click", function (event) {
   event.preventDefault();
-  slidePage.style.marginLeft = "-129.16%";
+  slidePage.style.marginLeft = "-206.9%";
   $("#nav-progress-bar").addClass("navbar-disable-address");
 });
 
-prevBtnSec.addEventListener("click", function(event){
+prevBtnSec.addEventListener("click", function (event) {
   event.preventDefault();
-  slidePage.style.marginLeft = "-22.2%";
+  slidePage.style.marginLeft = "-88.86%";
   bullet[current - 2].classList.remove("active");
   progressCheck[current - 2].classList.remove("active");
   progressText[current - 2].classList.remove("active");
@@ -71,9 +114,9 @@ prevBtnSec.addEventListener("click", function(event){
   $(".second-bullet-status").removeClass("bullet-first");
   $(".second-para-status").removeClass("step-first");
 });
-prevBtnThird.addEventListener("click", function(event){
+prevBtnThird.addEventListener("click", function (event) {
   event.preventDefault();
-  slidePage.style.marginLeft = "-54.16%";
+  slidePage.style.marginLeft = "-131.92%";
   bullet[current - 2].classList.remove("active");
   progressCheck[current - 2].classList.remove("active");
   progressText[current - 2].classList.remove("active");
@@ -81,9 +124,9 @@ prevBtnThird.addEventListener("click", function(event){
   $(".third-bullet-status").removeClass("bullet-first");
   $(".third-para-status").removeClass("step-first");
 });
-prevBtnFourth.addEventListener("click", function(event){
+prevBtnFourth.addEventListener("click", function (event) {
   event.preventDefault();
-  slidePage.style.marginLeft = "-79.16%";
+  slidePage.style.marginLeft = "-156.925%";
   bullet[current - 2].classList.remove("active");
   progressCheck[current - 2].classList.remove("active");
   progressText[current - 2].classList.remove("active");
@@ -95,21 +138,21 @@ prevBtnFourth.addEventListener("click", function(event){
 // -------------------- checkbox selected -----------------------------------
 
 function selectOnlyThis(id) {
-    var myCheckbox = document.getElementsByName("myCheckbox");
-    Array.prototype.forEach.call(myCheckbox, function (el) {
-      el.checked = false;
-    });
-    id.checked = true;   
+  var myCheckbox = document.getElementsByName("myCheckbox");
+  Array.prototype.forEach.call(myCheckbox, function (el) {
+    el.checked = false;
+  });
+  id.checked = true;
 }
 
-function offLine(id){
+function offLine(id) {
   selectOnlyThis(id);
   var OfflineSelect = document.getElementById("offline").value;
   console.log(OfflineSelect);
   $("#detail-address").removeClass("disable-address");
 }
 
-function onLine(id){
+function onLine(id) {
   selectOnlyThis(id);
   var OnlineSelect = document.getElementById("online").value;
   $("#detail-address").addClass("disable-address");
@@ -119,13 +162,12 @@ function onLine(id){
   console.log(OnlineSelect);
 }
 
-
 function selectOnlyDetailAddress(id) {
   var myCheckbox = document.getElementsByName("detailAddress");
   Array.prototype.forEach.call(myCheckbox, function (el) {
     el.checked = false;
   });
-  id.checked = true;   
+  id.checked = true;
 }
 
 function aToB(id) {
@@ -151,3 +193,64 @@ function addressFullCity(id) {
   $("#detail-fullcity").addClass("enable-address");
   console.log("fullCity");
 }
+
+// ---------------------------------- custome checkbox start --------------------------
+const dummyCheck = true;
+const checkbox1 = document.getElementById("details-1");
+checkbox1.checked = dummyCheck;
+checkbox1.addEventListener("change", (event) => {
+  if (event.currentTarget.checked) {
+    console.log(event.target.value);
+  }
+});
+const checkbox2 = document.getElementById("details-2");
+checkbox2.checked = dummyCheck;
+checkbox2.addEventListener("change", (event) => {
+  if (event.currentTarget.checked) {
+    console.log(event.target.value);
+  }
+});
+const checkbox3 = document.getElementById("details-3");
+checkbox3.addEventListener("change", (event) => {
+  if (event.currentTarget.checked) {
+    console.log(event.target.value);
+  }
+});
+const checkbox4 = document.getElementById("details-4");
+checkbox4.addEventListener("change", (event) => {
+  if (event.currentTarget.checked) {
+    console.log(event.target.value);
+  }
+});
+
+// ---------------------------------- custome checkbox end --------------------------
+// ---------------------------------- OTP Count start --------------------------
+function startOtpTimer() {
+  var counter = 6;
+  setInterval(function () {
+    counter--;
+    if (counter >= 0) {
+      span = document.getElementById("otp-count");
+      span.innerHTML = `00:${counter} sec`;
+    }
+    if (counter === 0) {
+      document.getElementById("resend-otp").style = "cursor: pointer;";
+      document.getElementById("otp-count").style = "color:red;";
+      // alert("sorry, out of time");
+      clearInterval(counter);
+    }
+  }, 1000);
+}
+function OTPCount() {
+  document.getElementById("otp-count").style = "color:green;";
+  document.getElementById("resend-otp").style =
+    "pointer-events: none; cursor: not-allowed;";
+  const mobileNumber = document.getElementById("mobile-input").value;
+  console.log("mobileNumber", mobileNumber);
+  startOtpTimer();
+  fetch(`http://65.0.205.205:8080/api/vendors/send_otp_sms/${mobileNumber}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+
+// ---------------------------------- OTP Count end --------------------------
