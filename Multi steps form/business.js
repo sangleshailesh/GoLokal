bd800a18b3f46950a2dcd65d75a714b3c5d6aafd
Shailesh Sangle <sangleshailesh30@gmail.com>
2022-01-23 13:29:07 +0530

    add OTP and validation API call

--- Multi steps form/business.js
+++ Multi steps form/business.js
@@ -27,16 +27,49 @@ welcomeStart.addEventListener("click", function (event) {
   // $(".first-bullet-status").addClass("bullet-first");
   // $(".first-para-status").addClass("step-first");
 });
+
+var mobileNumber;
 mobileSubmit.addEventListener("click", function (event) {
   event.preventDefault();
-  slidePage.style.marginLeft = "-30.7%";
-  // bullet[current - 1].classList.add("active");
-  // progressCheck[current - 1].classList.add("active");
-  // progressText[current - 1].classList.add("active");
-  // current += 1;
-  // $("#nav-progress-bar").removeClass("navbar-disable-address");
-  // $(".first-bullet-status").addClass("bullet-first");
-  // $(".first-para-status").addClass("step-first");
+  console.log("hiiii");
+  // function OTPCount() {
+  var text = document.getElementById("mobile-input").value;
+  var regx = /^[6-9]\d{9}$/;
+  if (!regx.test(text)) {
+    alert("Please Enter Valid Number");
+  } else {
+    function startOtpTimer() {
+      var counter = 60;
+      setInterval(function () {
+        counter--;
+        if (counter >= 0) {
+          span = document.getElementById("otp-count");
+          span.innerHTML = `00:${counter} sec`;
+        }
+        if (counter === 0) {
+          document.getElementById("resend-otp").style = "cursor: pointer;";
+          document.getElementById("otp-count").style = "color:red;";
+          // alert("sorry, out of time");
+          clearInterval(counter);
+        }
+      }, 1000);
+    }
+    // function OTPCount() {
+    document.getElementById("otp-count").style = "color:green;";
+    document.getElementById("resend-otp").style =
+      "pointer-events: none; cursor: not-allowed;";
+    mobileNumber = document.getElementById("mobile-input").value;
+    console.log("mobileNumber", mobileNumber);
+    startOtpTimer();
+    fetch(`http://65.0.133.233:8080/api/vendors/send_otp_sms/${mobileNumber}`, {
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+
+    slidePage.style.marginLeft = "-30.7%";
+  }
+  // }
 });
 
 backButton.addEventListener("click", function (event) {
@@ -47,13 +80,19 @@ backButton.addEventListener("click", function (event) {
 otpSubmit.addEventListener("click", function (event) {
   event.preventDefault();
   slidePage.style.marginLeft = "-54.4%";
-  // bullet[current - 1].classList.add("active");
-  // progressCheck[current - 1].classList.add("active");
-  // progressText[current - 1].classList.add("active");
-  // current += 1;
-  // $("#nav-progress-bar").removeClass("navbar-disable-address");
-  // $(".first-bullet-status").addClass("bullet-first");
-  // $(".first-para-status").addClass("step-first");
+  const UserOtpNumber = document.getElementById("first-digit").value;
+
+  fetch(`http://65.0.133.233:8080/api/vendors/validate_otp`, {
+    method: "POST",
+    body: {
+      otp: UserOtpNumber,
+      mob_number: mobileNumber,
+    },
+  })
+    //  read otp and mobile number from body pass into body comment above link
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  console.log("UserOtpNumber", UserOtpNumber);
 });
 letsStart.addEventListener("click", function (event) {
   event.preventDefault();
@@ -248,9 +287,100 @@ function OTPCount() {
   const mobileNumber = document.getElementById("mobile-input").value;
   console.log("mobileNumber", mobileNumber);
   startOtpTimer();
-  fetch(`http://65.0.205.205:8080/api/vendors/send_otp_sms/${mobileNumber}`)
+  fetch(`http://65.0.133.233:8080/api/vendors/send_otp_sms/${mobileNumber}`, {
+    method: "POST",
+  })
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
 
 // ---------------------------------- OTP Count end --------------------------
+
+// ------------------------------------- Category Selector Dropdown start-----------------------------
+function toggleDropdown() {
+  var menuBox = document.getElementById("dropdownList");
+  if (menuBox.style.display == "block") {
+    // if is menuBox displayed, hide it
+    menuBox.style.display = "none";
+  } else {
+    // if is menuBox hidden, display it
+    menuBox.style.display = "block";
+  }
+}
+function toggleDropdown2() {
+  var menuBox2 = document.getElementById("dropdownList");
+  menuBox2.style.display = "none";
+}
+
+let obj = {
+  category1: false,
+  category2: false,
+  category3: false,
+  category4: false,
+  category5: false,
+  category6: false,
+  category7: false,
+  category8: false,
+  category9: false,
+  category10: false,
+  category11: false,
+};
+
+let dropSection = document.querySelector(".drop-section");
+dropSection.addEventListener("click", (e) => {
+  if (e.target.classList.contains("option")) {
+    let category = e.target.getAttribute("data-attribute");
+    if (e.target.classList.contains("display")) {
+      e.target.classList.remove("display");
+      obj[`${category}`] = false; // Yeh line naya add kiya hai
+    } else {
+      e.target.classList.add("display");
+      obj[`${category}`] = true; // Yeh line naya add kiya hai
+    }
+  }
+  console.log("obj", obj);
+});
+// ------------------------------------- Category Selector Dropdown end -------------------------------
+// ------------------------------------- Days Dropdown start -------------------------------
+function toggleDaysDropdown() {
+  var menuBox = document.getElementById("dropdownDaysList");
+  if (menuBox.style.display == "block") {
+    // if is menuBox displayed, hide it
+    menuBox.style.display = "none";
+  } else {
+    // if is menuBox hidden, display it
+    menuBox.style.display = "block";
+  }
+}
+function toggleDaysDropdown2() {
+  var menuBox2 = document.getElementById("dropdownDaysList");
+  menuBox2.style.display = "none";
+  console.log(document.getElementById("start").value);
+  console.log(document.getElementById("end").value);
+}
+
+let selectedDays = {
+  dayOne: false,
+  dayTwo: false,
+  dayThree: false,
+  dayFour: false,
+  dayFive: false,
+  daySix: false,
+  daySeven: false,
+};
+
+let daysSection = document.querySelector(".days-section");
+daysSection.addEventListener("click", (e) => {
+  if (e.target.classList.contains("days")) {
+    let category = e.target.getAttribute("data-attribute");
+    if (e.target.classList.contains("display")) {
+      e.target.classList.remove("display");
+      selectedDays[`${category}`] = false; // Yeh line naya add kiya hai
+    } else {
+      e.target.classList.add("display");
+      selectedDays[`${category}`] = true; // Yeh line naya add kiya hai
+    }
+  }
+  console.log("selectedDays", selectedDays);
+});
+// ------------------------------------- Days Dropdown end -------------------------------
